87a9455ffd01ac9cf4c6ae6150d86f0c67e167cd
Gabriel Wu <gabrielwu84@gmail.com>
2017-07-12 23:08:59 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+require.config({ urlArgs: "v=" +  (new Date()).getTime() });
+
+require.config({
+	packages:[
+		{"name":"appKernel","location":"../yvAppKernel"},
+		{"name":"ctype","location":"https://youvote.github.io/clicker-prod/ctype/"},
+	],
+	paths:{
+		"jquery":"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min",
+		"vue":"https://cdnjs.cloudflare.com/ajax/libs/vue/2.3.3/vue",
+	},
+});
+
+require(["appKernel"],function(appKernel){
+	var lessonId=$_GET("lessonid");
+	var studentName=$_GET("studentname");
+	var deviceUuid=device.uuid;
+	if(studentName==undefined){studentName=deviceUuid;}
+	var widFrame=document.getElementById("widFrame")
+	var submitBtn=document.getElementById("submitBtn")
+	var youVote=new appKernel(lessonId,studentName,deviceUuid);
+	youVote.setKernelParam("widFrame",widFrame);
+	youVote.setKernelParam("submitBtn",submitBtn);
+	youVote.setKernelParam("onConnectFail",function(errmsg){
+		console.log(errmsg)
+	})
+	youVote.connect();
+})
