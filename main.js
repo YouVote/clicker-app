74f7ef93ccbec500004a642e0bfe71fd14c6cbc7
Gabriel Wu <gabrielwu84@gmail.com>
2018-05-28 13:22:47 +0800

    reworked socketio-server routing system

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ require.config({
 	paths:{
 		"jquery":"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min",
 		"vue":"https://cdnjs.cloudflare.com/ajax/libs/vue/2.3.3/vue",
+		"socket-router":"https://youvote.github.io/socket-router/main",
 	},
 });
 
